a41fc2421a995b0fc3537cf5cb76956d6bbcd5a4
Akobir Tursunov <97350561+AkobirDev@users.noreply.github.com>
2023-02-13 14:18:28 +0500

    add users.views by viewset

--- users/views.py
+++ users/views.py
@@ -1,4 +1,55 @@
 from django.shortcuts import render
-# from rest_framework import viewsets
+from django.contrib.auth import get_user_model
 
-# class UserView(viewsets.ViewSet)
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from users.permissions import IsUser, ReadOnly
+from users.serializers import UserSerializer
+
+User = get_user_model()
+
+class UserView(viewsets.ViewSet):
+    
+
+    def list(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk):
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def partial_update(self, request, pk):
+        user = User.objects.get(pk=pk)
+        instanse = user
+        serializer = UserSerializer(instanse, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsUser | ReadOnly]
+
+        return [permission() for permission in permission_classes]
+            
