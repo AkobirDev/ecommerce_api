782d46baade026cd4c1c2f0a9a31745dd214f3c5
Akobir Tursunov <97350561+AkobirDev@users.noreply.github.com>
2023-02-14 21:13:05 +0500

    add product testcase

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from products.models import Category, Product
+
+from users.models import CustomUser
 
 # Create your tests here.
+class ProductsTestCase(TestCase):
+
+    def test_add_product(self):
+        c = Category.objects.create(name='catg')
+        c.save()
+        
+        Product.objects.create(
+            name="smth",
+            category=c,
+            price=12,
+            discount = 0,
+        )
+        p = Product.objects.get(name='smth')
+
+        self.assertEqual(p.name, 'smth')
+        self.assertEqual(p.category, c)
+        self.assertEqual(p.price, 12)
+        self.assertEqual(p.discount, 0)
